5939bdb1cdab8ea453e1f219fffaf6f5d92c7554
janpoupa <poupajenda@gmail.com>
2025-04-22 00:11:51 +0200

    Install vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config'
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   site: 'https://janpoupa.com',
   base: '/goglance',
+  adapter: vercel(),
 })
